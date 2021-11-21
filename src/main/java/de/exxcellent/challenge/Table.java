3545f8263347fa51de1fccce726fb6b1e6d8f96f
Schapat <patrick.scharnow@gmx.de>
2021-11-21 21:14:38 +0100

    Create Table.java
    create Table class

---
+++ src/main/java/de/exxcellent/challenge/Table.java
@@ -0,0 +1,14 @@
+package de.exxcellent.challenge;
+
+/**
+ * This class creates table objects. 
+ * Each object contains a row with the identifier.
+ * @author Patrick Scharnow <patrick.scharnow@gmx.de>
+ */
+public class Table  {
+
+	public Table() {
+
+	}
+
+}
