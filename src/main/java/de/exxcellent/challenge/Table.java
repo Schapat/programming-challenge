486b7fd4c74a8f2e88dcfce53247c8ae853cc943
Schapat <patrick.scharnow@gmx.de>
2021-11-21 21:31:15 +0100

    Update Table.java
    add +Table constructor now fills HashMap + doku

--- src/main/java/de/exxcellent/challenge/Table.java
+++ src/main/java/de/exxcellent/challenge/Table.java
@@ -1,5 +1,7 @@
 package de.exxcellent.challenge;
 
+import java.util.HashMap;
+import java.util.List;
 /**
  * This class creates table objects. 
  * Each object contains a row with the identifier.
@@ -7,8 +9,23 @@ package de.exxcellent.challenge;
  */
 public class Table  {
 
-	public Table() {
-
+	private HashMap<String, String> row;
+	/**
+	 * Constructor of the class
+	 * Build a HashMap with the TableHeaders as Key and the rowData as value
+	 * @param identifiers are the Tableheaders
+	 * @param rowData Array contains data from table row
+	 */
+	public Table(String[] identifieres, String[] rowData) {
+		this.row = new HashMap<String,String>();
+		
+		for(int i = 0; i < identifieres.length; i++) {
+			this.row.put(identifieres[i], rowData[i]);
+		}
+	}	
+	
+	public HashMap<String, String> getRow() {
+		return row;
 	}
 
 }
