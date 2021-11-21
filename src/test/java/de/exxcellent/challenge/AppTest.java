59206f5c23b699f1c7a8146f0f9abb9b4d483f26
Schapat <patrick.scharnow@gmx.de>
2021-11-21 22:45:33 +0100

    testing
    test FindRowWithSmalestSpread

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,31 +1,45 @@
 package de.exxcellent.challenge;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Example JUnit 5 test case.
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
-
-    private String successLabel = "not successful";
-
-    @BeforeEach
-    void setUp() {
-        successLabel = "successful";
+	List<Table> rows = new ArrayList<Table>();
+	
+	HashMap<String, String> monday = new HashMap<String, String>();
+	HashMap<String, String> friday = new HashMap<String, String>();
+	
+	@BeforeEach                                         
+    void setUp() throws IOException {
+		//simulate readLine()
+		String[]identifieres = {"day", "minValue", "maxValue"};
+		String[]rowDataMonday = {"Monday", "1", "3"};
+		String[]rowDataFriday = {"Friday", "2", "10"};
+		//create Table obj
+		Table monday = new Table(identifieres, rowDataMonday);
+		Table friday = new Table(identifieres, rowDataFriday);
+	    //add to Table List
+		rows.add(monday);
+		rows.add(friday);
     }
 
-    @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
-    }
+	@Test
+	@DisplayName("Find Team with smallest spread (minValue/maxValue)") 
+	void testFindRowWithSmalestSpread() {
+	assertEquals("Monday",Calculator.calcMinSpread(rows, "minValue", "maxValue", "day"));
+	}
 
-    @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
-    }
 
 }
