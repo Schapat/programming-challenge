7736d37974fe08a9c60a82eb9dde1f674c2c3458
Schapat <patrick.scharnow@gmx.de>
2021-11-21 23:02:23 +0100

    Update App.java
    enter results

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,7 @@
 package de.exxcellent.challenge;
 
+import java.io.IOException;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -11,16 +13,18 @@ public final class App {
     /**
      * This is the main entry method of your program.
      * @param args The CLI arguments passed
+     * @throws IOException 
      */
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
 
         // Your preparation code …
-        // test commit
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+    	String weatherPath ="src\\main\\resources\\de\\exxcellent\\challenge\\weather.csv";
+    	String footballPath ="src\\main\\resources\\de\\exxcellent\\challenge\\football.csv";
+    	
+        String dayWithSmallestTempSpread = Calculator.calcMinSpread(CSVReader.reader(weatherPath), "MnT", "MxT", "Day");     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        String teamWithSmallestGoalSpread = Calculator.calcMinSpread(CSVReader.reader(footballPath),"Goals", "Goals Allowed", "Team"); // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
