28e57a6fcbed45858936bfc1fd1568dd8de2d267
Schapat <patrick.scharnow@gmx.de>
2021-11-21 21:41:42 +0100

    Update IReader.java
    add + doku

--- src/main/java/de/exxcellent/challenge/IReader.java
+++ src/main/java/de/exxcellent/challenge/IReader.java
@@ -1,7 +1,10 @@
 package de.exxcellent.challenge;
 
 import java.util.List;
-
+/**
+ * Readerinterface for reading tables. xml, csv, json etc..
+ * @author Patrick Scharnow <patrick.scharnow@gmx.de>
+ */
 public interface IReader {
 
 	static List<Table> reader() {
